69ea0e0f3482d9371fb402b488faaa8b79eb4d28
radhikashashi <radhikashashi24@gmail.com>
2025-02-01 10:20:05 +0530

    Hello

---
+++ PDemo001.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class PDemo001 {
+    String Cust_Name = "Shashank";
+    long Account_no = 123456;
+    Double Balance = 10000.00;
+
+    void accept_details() {
+        System.out.println("Enter your name and account number:");
+        Scanner sc = new Scanner(System.in);
+        Cust_Name = sc.nextLine();
+        Account_no = sc.nextLong();
+    
+    }
+
+    void Display_details() {
+        System.out.println("Hello " + Cust_Name);
+        System.out.println("Your balance is " + Balance);
+    }
+
+    public static void main(String[] args) {
+        PDemo001 obj = new PDemo001();
+        obj.accept_details();
+        obj.Display_details();
+    }
+}
